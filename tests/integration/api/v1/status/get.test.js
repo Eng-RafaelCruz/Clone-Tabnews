cc1876d9a30ad59215372041334bc3c99038d76f
Rafael Murilo dos Santos Cruz <140197758+Eng-RafaelCruz@users.noreply.github.com>
2025-03-02 19:51:19 +0000

    API comstrando conexões máximas e conexões abertas no momento

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("GET to api/v1/status shoul return 200", async () => {
+test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
@@ -9,4 +9,6 @@ test("GET to api/v1/status shoul return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.6");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
